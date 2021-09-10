2daeace516b0b0f6c220bb3179339f7571f53e2b
Masoom Kumar <masoom.kr04@gmail.com>
2021-09-11 03:17:43 +0530

    formatted pages

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,75 +1,77 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 //Animation
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 import logo from "../img/logo.svg";
-import { fetchSearch } from '../redux/actions/gamesAction';
+import { fetchSearch } from "../redux/actions/gamesAction";
 import { fadeIn } from "../animations";
 
 export const Nav = () => {
-    const dispatch = useDispatch();
-    const [textInput, setTextInput] = useState("");
+  const dispatch = useDispatch();
+  const [textInput, setTextInput] = useState("");
 
-    const inputHandler = (e) => {
-        setTextInput(e.target.value)
-    }
+  const inputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
 
-    const submitSearch = (e) => {
-        e.preventDefault();
-        dispatch(fetchSearch(textInput));
-        setTextInput("");
-    }
+  const submitSearch = (e) => {
+    e.preventDefault();
+    dispatch(fetchSearch(textInput));
+    setTextInput("");
+  };
 
-    const clearSearch = () => {
-        dispatch({
-            type : "CLEAR_SEARCHED",
-        })
-    }
+  const clearSearch = () => {
+    dispatch({
+      type: "CLEAR_SEARCHED",
+    });
+  };
 
-    return (
-        <StyledNav variants={fadeIn} initial="hidden" animate="show">
-            <Logo onClick={clearSearch}>
-                <img src={logo} alt="logo" />
-                <h1>Ignite</h1>
-            </Logo>
-            <form className="search">
-                <input value={textInput} type="text" onChange={inputHandler}/>
-                <button type="submit" onClick={submitSearch}>Search</button>
-            </form>
-        </StyledNav>
-    );
+  return (
+    <StyledNav variants={fadeIn} initial="hidden" animate="show">
+      <Logo onClick={clearSearch}>
+        <img src={logo} alt="logo" />
+        <h1>Ignite</h1>
+      </Logo>
+      <form className="search">
+        <input value={textInput} type="text" onChange={inputHandler} />
+        <button type="submit" onClick={submitSearch}>
+          Search
+        </button>
+      </form>
+    </StyledNav>
+  );
 };
 
 const StyledNav = styled(motion.nav)`
-    padding: 3rem 5rem;
-    text-align: center;
-    input {
-        width: 30%;
-        font-size: 1.5rem;
-        padding: 0.5rem;
-        border: none;
-        margin-top: 1rem;
-        box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
-        outline: none;
-    }
-    button {
-        font-size: 1.5rem;
-        border: none;
-        padding: 0.5rem 2rem;
-        cursor: pointer;
-        background: #ff7658;
-        color: white;
-    }
+  padding: 3rem 5rem;
+  text-align: center;
+  input {
+    width: 30%;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    border: none;
+    margin-top: 1rem;
+    box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
+    outline: none;
+  }
+  button {
+    font-size: 1.5rem;
+    border: none;
+    padding: 0.5rem 2rem;
+    cursor: pointer;
+    background: #ff7658;
+    color: white;
+  }
 `;
 
 const Logo = styled(motion.div)`
-    display: flex;
-    justify-content: center;
-    padding: 1rem;
-    cursor: pointer;
-    img {
-        width: 2rem;
-        height: 2rem;
-    }
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  cursor: pointer;
+  img {
+    width: 2rem;
+    height: 2rem;
+  }
 `;
